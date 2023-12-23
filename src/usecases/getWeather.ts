ebe0a37b8f00c6e31d159088af56920bddbf29ef
vntero <shin.antero@gmail.com>
2023-12-23 20:21:22 +0100

    finishes implementing the use case

--- src/usecases/getWeather.ts
+++ src/usecases/getWeather.ts
@@ -8,9 +8,20 @@ dotenv.config()
 const apiKey = process.env.WEATHER_API_KEY as string
 const apiUrl = new URL('http://api.weatherapi.com/v1/current.json')
 
-const getWeather = async (city: string) => {
+export const getWeather = async (city: string) => {
   try {
+    apiUrl.searchParams.append('key', apiKey)
+    apiUrl.searchParams.append('q', city)
 
+    const response = await fetch(apiUrl.toString())
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    console.error('🔥🔥🔥 ~ file: getWeather.ts:23 ~ getWeather ~ data:', data)
+    return data
   } catch (error) {
     console.error('Unable to fetch weather data', error)
   }
