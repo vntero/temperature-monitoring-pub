0e994f9926b118504cdf3eaf2fe43b7cea35ba22
vntero <shin.antero@gmail.com>
2023-12-23 18:18:58 +0100

    adds boilerplate ts configs

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const { defaults } = require('jest-config')
+module.exports = {
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  verbose: true,
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^mongoose$': '<rootDir>/node_modules/mongoose',
+  },
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx', 'js'],
+  modulePathIgnorePatterns: ['dist'],
+}
