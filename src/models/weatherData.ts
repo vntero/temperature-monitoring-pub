713f9712ef118ef93d2282de580f5a63d3a50b14
vntero <shin.antero@gmail.com>
2023-12-26 19:47:50 +0100

    adds models for response type

---
+++ src/models/weatherData.ts
@@ -0,0 +1,47 @@
+interface Location {
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  tzId: string;
+  localtimeEpoch: number;
+  localtime: string;
+}
+
+interface Condition {
+  text: string;
+  icon: string;
+  code: number;
+}
+
+interface Current {
+  lastUpdatedEpoch: number;
+  lastUpdated: string;
+  tempC: number;
+  tempF: number;
+  isDay: number;
+  condition: Condition;
+  windMph: number;
+  windKph: number;
+  windDegree: number;
+  windDir: string;
+  pressureMb: number;
+  pressureIn: number;
+  precipMm: number;
+  precipIn: number;
+  humidity: number;
+  cloud: number;
+  feelslikeC: number;
+  feelslikeF: number;
+  visKm: number;
+  visMiles: number;
+  uv: number;
+  gustMph: number;
+  gustKph: number;
+}
+
+export interface WeatherData {
+  location: Location;
+  current: Current;
+}
--- src/usecases/getCurrentWeather.ts
+++ src/usecases/getCurrentWeather.ts
@@ -1,4 +1,6 @@
 import * as dotenv from 'dotenv'
+
+import { WeatherData } from '../models/weatherData'
 dotenv.config()
 
 // ----- VARIABLES DECLARATION -----
@@ -6,7 +8,7 @@ const apiKey = process.env.WEATHER_API_KEY as string
 const apiUrl = new URL('http://api.weatherapi.com/v1/current.json')
 
 // ----- MAIN FUNCTION -----
-export const getCurrentWeather = async (city: string): Promise<any> => {
+export const getCurrentWeather = async (city: string): Promise<WeatherData> => {
   try {
     // adds key to the api call
     apiUrl.searchParams.append('key', apiKey)
@@ -16,14 +18,18 @@ export const getCurrentWeather = async (city: string): Promise<any> => {
     // fetches the data
     const response = await fetch(apiUrl.toString())
 
-    // handles the situation in case of an error
+    // handles the situation in case of an HTTP error
     if (!response.ok) {
       const errorData = await response.json()
       throw new Error(`HTTP error! Status: ${response.status}, Error: ${JSON.stringify(errorData)}`)
     }
 
-    // parses the data to json and returns it
-    return await response.json()
+    // parses data to json
+    const data = await response.json()
+    // logs data to the console
+    console.log(data)
+
+    return data
   } catch (error) {
     console.error('Unable to fetch weather data', error)
     throw error
