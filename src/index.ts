6a29103b8fc51fa4e9be19010f0227db3197e0f5
vntero <shin.antero@gmail.com>
2023-12-28 09:41:17 +0100

    adds mqtt configs

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,24 @@ import * as mqtt from 'mqtt'
 import { getCurrentWeather } from './usecases/getCurrentWeather'
 import { parseWeatherShort } from './usecases/parseWeatherShort'
 
+// ----- broker connection configs -----
 const mqttUrl = 'mqtt://localhost:1883'
 const client = mqtt.connect(mqttUrl)
 
+client.on('connect', () => {
+  console.log('Connected to mqtt broker')
+
+  client.subscribe('test-topic', (err) => {
+    if (!err) {
+      console.log('Subscribed to topic')
+    }
+  })
+})
+
+/**
+ * Combines getCurrentWeather with parseWeatherShort.
+ * It gets weather data and parses it to a short version.
+ */
 const getAndParseWeather = async () => {
   try {
     const weatherDataA = await getCurrentWeather('Lisbon')
