5fc6ff651bd0083a0b0a03c2b6bd2235ca2b68f3
vntero <shin.antero@gmail.com>
2023-12-28 15:44:22 +0100

    simplifies get weather data fn

--- apps/publisher/src/index.ts
+++ apps/publisher/src/index.ts
@@ -1,49 +1,22 @@
 import * as mqtt from 'mqtt'
 
 import { getCurrentWeather } from './usecases/getCurrentWeather'
-import { parseWeatherShort } from './usecases/parseWeatherShort'
 
 // ----- broker connection configs -----
-const mqttUrl = 'http://localhost:1883/'
-const client = mqtt.connect(mqttUrl)
-const topic = 'test-topic'
+const client = mqtt.connect('mqtt://broker.hivemq.com/')
+const topic = 'candide-cunegonde'
 
+// ----- connect to broker -----
 client.on('connect', () => {
   console.log('Connected to mqtt broker')
 
-  client.subscribe(topic, (err) => {
-    if (!err) {
-      console.log('Subscribed to topic')
-    } else {
-      console.error('Error subscribing to topic:', err)
-    }
-  })
-})
-
-// ----- gets weather data and parses it to short version -----
-const getAndPublishWeather = async () => {
-  try {
+  setInterval(async () => {
     const weatherDataA = await getCurrentWeather('Lisbon')
-    const weatherShortA = parseWeatherShort(weatherDataA)
-    console.log(weatherShortA)
-
+    console.log(weatherDataA)
     const weatherDataB = await getCurrentWeather('Zurich')
-    const weatherShortB = parseWeatherShort(weatherDataB)
-    console.log(weatherShortB)
+    console.log(weatherDataB)
 
-    // publish messages to the topic
-    const messages = [weatherShortA, weatherShortB]
-    client.publish('test-topic', JSON.stringify(messages), (err) => {
-      if (!err) {
-        console.log('Messages published:', messages)
-      } else {
-        console.error('Error publishing messages:', err)
-      }
-    })
-  } catch (error) {
-    console.error('Error getting or parsing weather data:', error)
-  }
-}
-
-// ----- calls the function every 5 seconds -----
-setInterval(getAndPublishWeather, 5000)
+    client.publish(topic, 'Simple MQTT using HiveMQ: ' + weatherDataA + '.')
+    client.publish(topic, 'Simple MQTT using HiveMQ: ' + weatherDataB + '.')
+  }, 5000)
+})
--- apps/publisher/src/usecases/getCurrentWeather.ts
+++ apps/publisher/src/usecases/getCurrentWeather.ts
@@ -1,13 +1,14 @@
 import * as dotenv from 'dotenv'
 
-import { WeatherData } from '../models/weatherData'
+import { WeatherData, WeatherDataShort } from '../models/weatherData'
+
 dotenv.config()
 
 /**
  * @param city 
  * @returns Current weather data in json
  */
-export const getCurrentWeather = async (city: string): Promise<WeatherData> => {
+export const getCurrentWeather = async (city: string): Promise<WeatherDataShort> => {
   try {
     const apiKey = process.env.WEATHER_API_KEY as string
     const apiUrl = new URL('http://api.weatherapi.com/v1/current.json')
@@ -15,18 +16,21 @@ export const getCurrentWeather = async (city: string): Promise<WeatherData> => {
     apiUrl.searchParams.append('key', apiKey)
     // adds city name to the api call
     apiUrl.searchParams.append('q', city)
-
     // fetches the data
     const response = await fetch(apiUrl.toString())
-
     // handles the situation in case of an HTTP error
     if (!response.ok) {
       const errorData = response.json()
       throw new Error(`HTTP error! Status: ${response.status}, Error: ${JSON.stringify(errorData)}`)
     }
-
     // parses data to json
-    return response.json()
+    const data = await response.json() as WeatherData
+    // returns required structure
+    return {
+      city: data.location.name,
+      temperature: data.current.temp_c,
+      date: new Date(data.location.localtime),
+    }
   } catch (error) {
     console.error('Unable to fetch weather data', error)
     throw error
