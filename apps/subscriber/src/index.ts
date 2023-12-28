e9970a77f045cfcd8a907731f318bcc700634f6a
vntero <shin.antero@gmail.com>
2023-12-28 14:49:48 +0100

    subscriber works as expected

--- apps/subscriber/src/index.ts
+++ apps/subscriber/src/index.ts
@@ -1,49 +1,21 @@
 import * as mqtt from 'mqtt'
 
-import { getCurrentWeather } from './usecases/getCurrentWeather'
-import { parseWeatherShort } from './usecases/parseWeatherShort'
+// broker connection configs
+const client = mqtt.connect('mqtt://broker.hivemq.com')
+const topic = 'candide-cunegonde'
 
-// ----- broker connection configs -----
-const mqttUrl = 'http://localhost:1883/'
-const client = mqtt.connect(mqttUrl)
-const topic = 'test-topic'
-
+// connects to broker
 client.on('connect', () => {
-  console.log('Connected to mqtt broker')
-
+  console.log('Connected to mqtt broker via HiveMQ')
+  // subscribes to topic
   client.subscribe(topic, (err) => {
-    if (!err) {
-      console.log('Subscribed to topic')
-    } else {
-      console.error('Error subscribing to topic:', err)
-    }
+    !err
+      ? console.log('Subscribed to topic: ' + topic)
+      : console.error('Error subscribing to topic:', err)
   })
 })
 
-// ----- gets weather data and parses it to short version -----
-const getAndPublishWeather = async () => {
-  try {
-    const weatherDataA = await getCurrentWeather('Lisbon')
-    const weatherShortA = parseWeatherShort(weatherDataA)
-    console.log(weatherShortA)
-
-    const weatherDataB = await getCurrentWeather('Zurich')
-    const weatherShortB = parseWeatherShort(weatherDataB)
-    console.log(weatherShortB)
-
-    // publish messages to the topic
-    const messages = [weatherShortA, weatherShortB]
-    client.publish('test-topic', JSON.stringify(messages), (err) => {
-      if (!err) {
-        console.log('Messages published:', messages)
-      } else {
-        console.error('Error publishing messages:', err)
-      }
-    })
-  } catch (error) {
-    console.error('Error getting or parsing weather data:', error)
-  }
-}
-
-// ----- calls the function every 5 seconds -----
-setInterval(getAndPublishWeather, 5000)
+// grabs message and logs it
+client.on('message', (topic, message) => {
+  console.log('Topic: ' + topic, 'Message: ' + message.toString())
+})
