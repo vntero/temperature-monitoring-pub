a64a2b4b2e61f636050a2607b807d64777fa8cd5
vntero <shin.antero@gmail.com>
2023-12-28 10:34:05 +0100

    messages are published successfully, moving on to sub service

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,12 @@ import { parseWeatherShort } from './usecases/parseWeatherShort'
 // ----- broker connection configs -----
 const mqttUrl = 'mqtt://localhost:1883'
 const client = mqtt.connect(mqttUrl)
+const topic = 'test-topic'
 
 client.on('connect', () => {
   console.log('Connected to mqtt broker')
 
-  client.subscribe('test-topic', (err) => {
+  client.subscribe(topic, (err) => {
     if (!err) {
       console.log('Subscribed to topic')
     }
@@ -18,19 +19,24 @@ client.on('connect', () => {
 })
 
 // ----- gets weather data and parses it to short version -----
-const getAndParseWeather = async () => {
+const getAndPublishWeather = async () => {
   try {
     const weatherDataA = await getCurrentWeather('Lisbon')
-    const parsedDataA = parseWeatherShort(weatherDataA)
-    console.log(parsedDataA)
+    const weatherShortA = parseWeatherShort(weatherDataA)
+    console.log(weatherShortA)
 
     const weatherDataB = await getCurrentWeather('Zurich')
-    const parsedDataB = parseWeatherShort(weatherDataB)
-    console.log(parsedDataB)
+    const weatherShortB = parseWeatherShort(weatherDataB)
+    console.log(weatherShortB)
+
+    // publish messages to the topic
+    client.publish(topic, JSON.stringify(weatherShortA))
+    client.publish(topic, JSON.stringify(weatherShortB))
+    console.log('Messages published')
   } catch (error) {
     console.error('Error getting or parsing weather data:', error)
   }
 }
 
 // ----- calls the function every 5 seconds -----
-setInterval(getAndParseWeather, 5000)
+setInterval(getAndPublishWeather, 5000)
