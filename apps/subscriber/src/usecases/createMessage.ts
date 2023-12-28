a6b2c4879accf6f6dce8838db92b1c76c8170ba7
vntero <shin.antero@gmail.com>
2023-12-28 17:05:11 +0100

    adds sequelize configs

--- apps/subscriber/src/usecases/createMessage.ts
+++ apps/subscriber/src/usecases/createMessage.ts
@@ -1,4 +1,11 @@
+import { Sequelize } from 'sequelize'
+import * as dotenv from 'dotenv'
+
 import { WeatherDataShort } from '../models/weatherData'
+
+dotenv.config()
+
+const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@example.com:5432/temperature-monitoring`)
 
 export const createMessage = (message: WeatherDataShort): any => {
 
