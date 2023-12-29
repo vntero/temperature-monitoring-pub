661e030a5bffa7e0ff09c4360838ac7a8c617c88
vntero <shin.antero@gmail.com>
2023-12-29 12:25:42 +0100

    tests db connections

--- apps/subscriber/src/usecases/createMessage.ts
+++ apps/subscriber/src/usecases/createMessage.ts
@@ -5,8 +5,26 @@ import { WeatherDataShort } from '../models/weatherData'
 
 dotenv.config()
 
-const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@example.com:5432/temperature-monitoring`)
+// Extract database connection information from environment variables
+const dbName = process.env.DB
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
 
-export const createMessage = (message: WeatherDataShort): any => {
+// Construct the connection URI
+const dbConnectionUri = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`
 
+// connect to database by passing a connection URI
+const sequelize = new Sequelize(dbConnectionUri)
+
+export const createMessage = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully')
+  } catch (error) {
+    console.error('Unable to connect to the database', error)
+  }
 }
+
+createMessage()
