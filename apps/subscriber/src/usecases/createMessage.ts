0d4777b0ccb319a0092925ebdc72a624644d027c
vntero <shin.antero@gmail.com>
2023-12-29 12:31:56 +0100

    changes comments on db connection

--- apps/subscriber/src/usecases/createMessage.ts
+++ apps/subscriber/src/usecases/createMessage.ts
@@ -5,14 +5,14 @@ import { WeatherDataShort } from '../models/weatherData'
 
 dotenv.config()
 
-// Extract database connection information from environment variables
+// extract db connection information from env file
 const dbName = process.env.DB
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
 const dbHost = process.env.DB_HOST
 const dbPort = process.env.DB_PORT
 
-// Construct the connection URI
+// construct the connection URI
 const dbConnectionUri = `postgres://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`
 
 // connect to database by passing a connection URI
